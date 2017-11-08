f0f71726f106f3fe7dcc40146c3e62694d2ce773
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-11-08 11:46:58 +0000

    Removed play functionality

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import Flask, request, redirect, url_for, render_template, flash
 from werkzeug.utils import secure_filename
 
 from dao import DAO
-from audioplay import Audio
 
 IMAGE_UPLOAD_FOLDER = 'static/images/'
 AUDIO_UPLOAD_FOLDER = 'audio'
@@ -17,55 +16,13 @@ app.config['IMAGE_UPLOAD_FOLDER'] = IMAGE_UPLOAD_FOLDER
 app.config['AUDIO_UPLOAD_FOLDER'] = AUDIO_UPLOAD_FOLDER
 app.config['SECRET_KEY'] = '\x0bE\x85\xed\xb0\xa5\xda\x90\xb2\x94\xd0\x02\x96\xda=\xf1\x83w\x9ei\xc3#|\xa2'
 
-#set up a global for audio. 
-state = 1
-curr_time = 0
-
-@app.route('/<uid_str>', methods=['GET', 'POST'])
+@app.route('/')
 def query_picture_position(uid_str):
     '''
-       Takes the (x,y) tuple from post and retrieves the audio if near
+       Place holder
     '''
-    global state
-    if request.method == 'GET':
-        return render_template('touch.html')
-    if request.method == 'POST':
-        content = request.get_json(force=True)
-        if state == 0:
-            if 'x' in content:
-                if content["x"] < 144:
-                   if content["y"] < 164:
-                    play('overview.mp3')
-                   elif content["y"] < 194:
-                    stop()
-                else:        
-                    pos_audio = check_audio(content["x"],content["y"])
-                    print(pos_audio)
-                    if pos_audio is not None:
-                        play(pos_audio)
-        else:
-            state = 0
-            stop()
-        return uid_str
+    return render_template('touch.html')
     
-
-def check_audio(x,y):
-    audio = DAO().fetch(x,y)
-    if audio is not 'None' and audio is not None:
-       return audio
-
-def play(filename):
-    if filename == "overview.mp3":
-        curr_time = time.time()
-        state == 3
-        Audio().play(filename)
-    else:
-        if time.time() > (curr_time + (2 * 60 * 1000)):
-            Audio().play(filename)
- 
-def stop():
-    Audio().stop()
-
 def allowed_file(filename, extension):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in extension
