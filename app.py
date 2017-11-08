fe0b8cf42158ef8b7e3328c4903662dc44fcf94c
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-11-08 16:05:38 +0000

    Updated file paths and loading around file name

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import json
 from flask import Flask, request, redirect, url_for, render_template, flash
 from werkzeug.utils import secure_filename
 
+from git import GitRepo
 from dao import DAO
 
-IMAGE_UPLOAD_FOLDER = 'static/images/'
-AUDIO_UPLOAD_FOLDER = 'audio'
+IMAGE_UPLOAD_FOLDER = 'static/tiles/'
+AUDIO_UPLOAD_FOLDER = 'static/tiles/audio'
 ALLOWED_EXTENSIONS_AUDIO = set(['mp3'])
 ALLOWED_EXTENSIONS_PIC = set(['jpg', 'png', 'jpeg', 'gif'])
 
@@ -17,7 +18,7 @@ app.config['AUDIO_UPLOAD_FOLDER'] = AUDIO_UPLOAD_FOLDER
 app.config['SECRET_KEY'] = '\x0bE\x85\xed\xb0\xa5\xda\x90\xb2\x94\xd0\x02\x96\xda=\xf1\x83w\x9ei\xc3#|\xa2'
 
 @app.route('/')
-def query_picture_position(uid_str):
+def query_picture_position():
     '''
        Place holder
     '''
@@ -45,26 +46,33 @@ def upload_file(uid):
             file.save(os.path.join(app.config['AUDIO_UPLOAD_FOLDER'], filename))
             DAO().insert_data(uid, filename, float(request.values["x"]), float(request.values["y"]))
             fname = None
-            for f in os.listdir(app.config['IMAGE_UPLOAD_FOLDER']):
+            dirname = os.path.join(app.config['IMAGE_UPLOAD_FOLDER'], uid)
+            for f in os.listdir(dirname):
                 if f[:len(uid)] == uid:
                     fname = f
             layout = None
-            with open(uid + '.json', 'rb') as fh:
-                _tmp = json.loads(f.read())
+            coords = None
+            with open(dirname + '/index.json', 'rb') as fh:
+                _tmp = json.loads(fh.read())
                 layout = _tmp['layout']
-            coords = [{'x':14,'y':44 }, {'x':14,'y':84 }]
-            coords = DAO().fetch_xy(uid)
+                coords = _tmp['points']
+            #coords = DAO().fetch_xy(uid)
             return render_template('record.html',layout=layout, record=fname, coords=coords)
 
     if request.method == 'GET':
         fname = None
-        for f in os.listdir(app.config['IMAGE_UPLOAD_FOLDER']):
+        dirname = os.path.join(app.config['IMAGE_UPLOAD_FOLDER'], uid)
+        for f in os.listdir(dirname):
             if f[:len(uid)] == uid:
-                fname = f
-        coords = [{'x':44,'y':44 }, {'x':44,'y':84 }]
-        #coords = DAO().fetch_xy(uid)
-        #coords.append({'x':0.00,'y':0.10 })
-        print(coords)
+                fname = dirname + '/'+f
+        layout = None
+        coords = None
+        with open(dirname + '/index.json', 'rb') as fh:
+            _tmp = json.loads(fh.read())
+            layout = _tmp['layout']
+            coords = _tmp['points']
+
+        print(fname)
         return render_template('record.html', record=fname, coords=coords)
 
 def get_records():
@@ -89,9 +97,12 @@ def get_files():
             return redirect(request.url)
         if file and allowed_file(file.filename, ALLOWED_EXTENSIONS_PIC):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['IMAGE_UPLOAD_FOLDER'], filename))
             fname = filename.split('.')
-            with open(fname[0] + '.json', 'wb') as f:
-                f.write(json.dumps({'layout': request.form['interest']}))
+            dirname = os.path.join(app.config['IMAGE_UPLOAD_FOLDER'], fname[0])
+            os.mkdir(dirname)
+            file.save(dirname + '/' +filename)
+            
+            with open(os.path.join(dirname, 'index.json'), 'wb') as f:
+                f.write(json.dumps({'layout': request.form['interest'], 'points': [{'x': 14, 'y': 44}, {'x': 14, 'y': 84}]}))
             return redirect(url_for('get_files'))
      
